78ff9a8b29f8e870f6fbba95d1843efc3b8716e4
romanmihailow <romanmihailow@email.com>
2023-01-26 19:59:29 +0500

    add directory resources and add file data.csv

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
+import ru.netology.bonus.BonusService;
+
 public class Main {
 
     public static void main(String[] args) {
         BonusService service = new BonusService(); // Сервис расчета бонусов
-        long bonus = service.calculate( 99, false);
+        long bonus = service.calculate( 100060, true);
         System.out.println(bonus);
 
 
--- src/main/java/ru/netology/bonus/BonusService.java
+++ src/main/java/ru/netology/bonus/BonusService.java
@@ -1,4 +1,8 @@
+package ru.netology.bonus;
+
 public class BonusService {
+
+
     public long calculate(long amount, boolean registered) {
         int percent = registered ? 3 : 1;
         long bonus = amount * percent / 100;
